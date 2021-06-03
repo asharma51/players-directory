02043543f4d979bb96790085b790053416b8dc19
Apoorva Sharma <apoorva@Apoorvas-MBP.hsd1.ca.comcast.net>
2021-06-03 13:45:06 -0700

    updated navbar in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component} from 'react';
 import {
   Collapse,
   Navbar,
@@ -13,46 +13,62 @@ import {
   DropdownItem
 } from 'reactstrap';
 
-const Example = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+export default class AppNavbar extends Component {
+	constructor(props) {
+		super(props);
 
-  const toggle = () => setIsOpen(!isOpen);
+		this.toggle = this.toggle.bind(this);
+		this.toggleCreatePlayerModal = this.toggleCreatePlayerModal.bind(this);
+		this.state = {
+			isOpen: false,
+			modal: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+	toggleCreatePlayerModal() {
+		this.setState({ modal: !this.state.modal })
+	}
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Tennis Players Directory</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/components/">Players</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+	render() {
+		return (
+			<div>
+				<Navbar color="dark" fixed="top" dark expand="md">
+					<NavbarBrand href="/"><img src="/assets/logo.jpg" alt="logo for players directory app" className="logo" />Players Directory</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="/">Home</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/players">Players</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavbar>
+								<DropdownToggle nav caret>
+									Options
+                </DropdownToggle>
+								<DropdownMenu right>
+									<DropdownItem >
+										Create Player
+                  </DropdownItem>
+									<DropdownItem>
+										Option 2
+                  </DropdownItem>
+									<DropdownItem divider />
+									<DropdownItem>
+										Reset
+                  </DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				
+				</Navbar>
+			</div>
+		);
+	}
 }
-
-export default Example;
