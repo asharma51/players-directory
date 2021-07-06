3b4e99aa372b9f2fe4c9e950a938fc2a452e64bf
Apoorva Sharma <apoorva@Apoorvas-MBP.hsd1.ca.comcast.net>
2021-07-06 00:13:53 -0700

    added app instruction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Jumbotron, Container} from 'reactstrap';
+import { Jumbotron, Container, ListGroupItem, ListGroup, Spinner} from 'reactstrap';
 import '../style/Stylesheet.css';
 
 const Example = (props) => {
   return (
-    <Jumbotron fluid className="bg-danger border border-dark text-light">
-      <Container fluid className="home">
+    <>
+      <Jumbotron fluid className="bg-danger border border-dark text-light">
+        <Container fluid className="home">
           <h1 className="display-3">Players Directory App</h1>
           <p className="blockquote">Implemented using <span className="text-dark"><b>React</b></span> backed by <span className="text-dark"><b>postgreSQL</b></span></p>
           <hr className="my-2" />
@@ -15,6 +16,18 @@ const Example = (props) => {
           </p> */}
         </Container>
       </Jumbotron>
+      <Container fluid className="home">
+        <h1><i>Things you can do in this app</i> ➡ </h1><br></br>
+        <ListGroup>
+          <ListGroupItem color="primary">Click on <b>Players</b> button in navigation bar to check out existing players 🎾	 in application</ListGroupItem>
+          <ListGroupItem color="secondary">To open an individual player's card, click on <b>View Player</b> button</ListGroupItem>
+          <ListGroupItem color="info">Current players can be updated by clicking on <b>Edit Player</b> button on individual player's card</ListGroupItem>
+          <ListGroupItem color="danger">Delete 🚫 an existing player by clicking on <b>Delete Player</b> button on individual player's card</ListGroupItem>
+          <ListGroupItem color="success">New players can be created via <b>Options</b> -> <b>Create New Player</b></ListGroupItem>
+        </ListGroup>
+      </Container>
+      </>
+
   );
 };
 
